44ae8c751c672134647c1db6c4e0737be9a95e2a
Grant Hecht <granthec@buffalo.edu>
2021-09-19 20:54:53 -0400

    Update to allow passing of errors from Julia

--- deps/mexjulia.cpp
+++ deps/mexjulia.cpp
@@ -51,8 +51,6 @@ void mexFunction(int nl, mxArray* pl[], int nr, const mxArray* pr[])
         char *image = nr > 3 && mxIsChar(pr[3]) ? mxArrayToString(pr[3]) : NULL;
         char *lib =   nr > 4 && mxIsChar(pr[4]) ? mxArrayToString(pr[4]) : NULL;
         //jl_options.handle_signals = JL_OPTIONS_HANDLE_SIGNALS_OFF;
-        jl_options_t jl_options;
-        jl_options.handle_signals = JL_OPTIONS_HANDLE_SIGNALS_OFF;
 #ifdef _OS_LINUX_
         if (!dlopen(lib, RTLD_LAZY | RTLD_GLOBAL))
         {
@@ -77,9 +75,10 @@ void mexFunction(int nl, mxArray* pl[], int nr, const mxArray* pr[])
   jl_value_t *e = jl_exception_occurred();
   if (e)
   {
-    const size_t len = 1024;
-    static char msg[len];
-    snprintf(msg, len, "Unhandled Julia exception: %s", jl_typeof_str(e));
+    //const size_t len = 1024;
+    //static char msg[len];
+    //snprintf(msg, len, "Unhandled Julia exception: %s", jl_typeof_str(e));
+    const char* msg = jl_string_ptr(jl_eval_string("sprint(showerror, ccall(:jl_exception_occurred, Any, ()))"));
     jl_exception_clear();
     mexErrMsgTxt(msg);
   }
